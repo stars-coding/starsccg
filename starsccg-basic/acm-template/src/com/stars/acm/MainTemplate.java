69cb5c43d0fb84f0cc32f7481d53410c089844a7
stars-coding <2715800477@qq.com>
2023-11-06 20:06:05 +0800

    013-构建 jar 包与命令行下生成代码

---
+++ starsccg-basic/acm-template/src/com/stars/acm/MainTemplate.java
@@ -0,0 +1,35 @@
+package com.stars.acm;
+
+import java.util.Scanner;
+
+/**
+ * ACM 输入模板（多数之和）
+ *
+ * @author stars
+ */
+public class MainTemplate {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+            // 读取输入的元素个数
+            int n = scanner.nextInt();
+            // 创建数组
+            int[] arr = new int[n];
+            // 读取数组
+            for (int i = 0; i < n; i++) {
+                arr[i] = scanner.nextInt();
+            }
+            // 计算数组元素之和
+            int sum = 0;
+            for (int num : arr) {
+                sum += num;
+            }
+            // 打印数组元素之和
+            System.out.println("计算之和：" + sum);
+
+
+        scanner.close();
+    }
+}
