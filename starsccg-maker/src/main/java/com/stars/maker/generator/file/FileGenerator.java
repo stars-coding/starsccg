1b2e7ab7cdbf0d0531d3326c4addc830f2c7a52a
stars-coding <2715800477@qq.com>
2023-11-15 09:53:57 +0800

    016-初始化代码生成器制作工具项目

---
+++ starsccg-maker/src/main/java/com/stars/maker/generator/file/FileGenerator.java
@@ -0,0 +1,58 @@
+package com.stars.maker.generator.file;
+
+import com.stars.maker.model.DataModel;
+import freemarker.template.TemplateException;
+
+import java.io.File;
+import java.io.IOException;
+
+/**
+ * 核心生成器
+ *
+ * @author stars
+ * @version 1.0
+ */
+public class FileGenerator {
+
+    /**
+     * 入口函数
+     *
+     * @param args
+     * @throws TemplateException
+     * @throws IOException
+     */
+    public static void main(String[] args) throws TemplateException, IOException {
+        // 创建数据模型对象
+        DataModel dataModel = new DataModel();
+        dataModel.setAuthor("stars");
+        dataModel.setLoop(false);
+        dataModel.setOutputText("求和结果：");
+        // 生成文件
+        FileGenerator.doGenerate(dataModel);
+    }
+
+    /**
+     * 生成文件
+     *
+     * @param model 数据模型对象
+     * @throws TemplateException
+     * @throws IOException
+     */
+    public static void doGenerate(Object model) throws IOException, TemplateException {
+        // 模块单独打开时，为当前模块根路径
+        String projectPath = System.getProperty("user.dir");
+        // 顶级项目的根路径
+        File parentFile = new File(projectPath).getParentFile();
+
+        // 生成静态文件
+        String inputStaticFilePath = new File(parentFile, "starsccg-demo-projects/acm-template").getAbsolutePath();
+        String outputStaticFilePath = projectPath;
+        StaticFileGenerator.copyFilesByHutool(inputStaticFilePath, outputStaticFilePath);
+
+        // 生成动态文件
+        String inputDynamicFilePath = projectPath + File.separator + "src/main/resources/templates/MainTemplate.java.ftl";
+        String outputDynamicFilePath = outputStaticFilePath + File.separator
+                + "acm-template/src/com/stars/acm/MainTemplate.java";
+        DynamicFileGenerator.doGenerate(inputDynamicFilePath, outputDynamicFilePath, model);
+    }
+}
---
+++ starsccg-maker/src/main/java/com/stars/maker/model/DataModel.java
@@ -0,0 +1,28 @@
+package com.stars.maker.model;
+
+import lombok.Data;
+
+/**
+ * 动态模版配置
+ *
+ * @author stars
+ * @version 1.0
+ */
+@Data
+public class DataModel {
+
+    /**
+     * 是否生成循环
+     */
+    private boolean loop = true;
+
+    /**
+     * 作者注释
+     */
+    private String author = "默认作者";
+
+    /**
+     * 输出信息
+     */
+    private String outputText = "默认输出信息";
+}
